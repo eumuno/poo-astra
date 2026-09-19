ca443155d3b839d2f70edc837a38f499ad363c8f
Isabeli Souza da Rosa <isabelisouza2609@gmail.com>
2026-09-19 15:23:38 -0300

    Criação da classe Usuário

---
+++ src/Usuario.java
@@ -0,0 +1,13 @@
+public class Usuario {
+
+    // ATRIBUTOS
+    private int id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public String getTipoUsuario() {
+        return null;
+    }
+
+}
